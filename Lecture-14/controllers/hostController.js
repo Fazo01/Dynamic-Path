3be5cdf12d6912af31906c235a0324fbda2a296c
Fazo01 <fg7829098@gmail.com>
2025-07-22 10:56:38 +0500

    Edit-Home: Showing existig data

--- Lecture-14/controllers/hostController.js
+++ Lecture-14/controllers/hostController.js
@@ -6,6 +6,7 @@ exports.getAddhome = (req, res, next) => {
   res.render("host/edit-home", {
     pageTitle: "Add Home",
     currentPage: "addHome",
+    editing:false,
   });
   //Important to change in partial
 };
@@ -13,13 +14,20 @@ exports.getAddhome = (req, res, next) => {
 exports.getEditHome = (req, res, next) => {
   const homeId = req.params.homeId;
   const editing = req.query.editing === "true";
-  console.log(homeId, editing);
-  res.render("host/edit-home", {
-    pageTitle: "Edit your Home",
-    currentPage: "host-home",
-    editing:editing,
+
+  Home.findById(homeId,home=>{//14 part 2
+    if(!home){
+      console.log("Home not found for editing")
+      return res.redirect("/host/host-home-list")
+    }
+    console.log(homeId, editing,home);
+    res.render("host/edit-home", {
+      home:home,
+      pageTitle: "Edit your Home",
+      currentPage: "host-home",
+      editing:editing,
+  })
   });
-  //Important to change in partial
 };
 
 exports.getHostHomes = (req, res, next) => {
--- Lecture-14/models/home.js
+++ Lecture-14/models/home.js
@@ -2,10 +2,9 @@ const fs = require("fs");
 const path = require("path");
 const rootDir = require("../utils/pathutils");
 
-    const homeDataPath = path.join(rootDir,'data', 'home.json');
+const homeDataPath = path.join(rootDir, "data", "home.json");
 //Fake database
 let registeredHomes = [];
-
 
 module.exports = class Home {
   constructor(houseName, price, location, rating, photoURL) {
@@ -18,30 +17,33 @@ module.exports = class Home {
   //To save Files
   save() {
     //Write File
-    this.id=Math.random().toString();
-    Home.fetchAll((registeredHomes)=>{
+    this.id = Math.random().toString();
+    Home.fetchAll((registeredHomes) => {
       registeredHomes.push(this);
-    fs.writeFile(homeDataPath, JSON.stringify(registeredHomes), error => {
-      
+      fs.writeFile(
+        homeDataPath,
+        JSON.stringify(registeredHomes),
+        (error) => {}
+      );
     });
-    })
   }
 
   //Read File
   static fetchAll(callback) {
-    const homeDataPath=path.join(rootDir,'data','home.json')
-    fs.readFile(homeDataPath,(err,data)=>{
-      if(!err){
-        callback(JSON.parse(data))
-      }else{
-      callback([]);
-    }
-    })
+    const homeDataPath = path.join(rootDir, "data", "home.json");
+    fs.readFile(homeDataPath, (err, data) => {
+      if (!err) {
+        callback(JSON.parse(data));
+      } else {
+        callback([]);
+      }
+    });
   }
-  static findById(homeId,callback){//I will study this
-    this.fetchAll(homes=>{
-      const homeFound=homes.find((home)=>home.id===homeId);//find method
+  static findById(homeId, callback) {
+    //I will study this
+    this.fetchAll((homes) => {
+      const homeFound = homes.find((home) => home.id === homeId); //find method
       callback(homeFound);
-    })
+    });
   }
 };
